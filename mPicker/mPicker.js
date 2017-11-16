d02d1c87e006bcb523e0478eb03e26c8c25286b4
David Harris <physicsdavid@gmail.com>
2017-11-17 09:22:26 +1000

    Improved documentation

--- mPicker/mPicker.js
+++ mPicker/mPicker.js
@@ -1,28 +1,20 @@
 /**
- * @file 3D Object picker for p5.js
+ * @file mPicker.js: 3D Object picker for p5.js
  * @author David Harris <physicsdavid@gmail.com>
  * @version 1.0
  *
- * This set of functions allows a simple-to-use implementation of 3D object picking for
- * beginning p5.js coders. It works by creating a hidden parallel 3D WEBGL model in which  
- * all objects in the visible model have corresponding objects with a color determined by
- * the object's assigned ID number. To identify an object the color of the hidden canvas
- * at a particular point then gives the ID number of the object closest to the camera at
- * that point.
+ * This set of functions allows a simple-to-use implementation of 3D object picking for beginning p5.js coders. It works by creating a hidden parallel 3D WEBGL model in which all objects in the visible model have corresponding objects with a color determined by the object's assigned ID number. To identify an object the color of the hidden canvas at a particular point then gives the ID number of the object closest to the camera at that point.
+ * The general principle of using the set of functions is to replace p5.js 3D functions with calls to these new functions, named by prepending the letter "m" to the method name and using camelCase capitalization. Methods that create 3D primitives require an extra parameter in the first position that assigns an ID number to that primitive. Multiple primitives can be assigned the same ID number to build up more complex objects that can be identified with a single ID number.
+
+ * Usage notes: 
+ * The module can be included in the head of an html document with a line such as `<script src="mPicker.js"></script>`. Alternatively, the contents of mPicker.js can be simply copied into a p5.js sketch.js document.
  *
- * The general principle of using the set of functions is to replace p5.js 3D methods with
- * calls to these functions, named by prepending the letter "m" to the method name and
- * using camelCase capitalization. Methods that create 3D primitives require an extra
- * parameter in the first position that assigns an ID number to that primitive. Multiple
- * primitives can be assigned the same ID number to build up more complex objects that
- * can be identified with a single ID number.
- * 
- * To identify an object based on the canvas position, use the function getObjectID().
+ * The p5.js draw() function should include the command mResetMatrix() before drawing any primitives to ensure proper synchronization of the hidden canvas with the visible canvas and accurate object identification.
  *
- * Important usage note: 
- * The p5.js draw() function should include the command mResetMatrix() to 
- * ensure proper synchronization of the hidden canvas with the visible canvas.
- * 
+ * The creation of primitives should include an additional parameter, first in the list, that assigns an ID number to the primitive. The ID can be any positive integer. Multiple primitives can be assigned the same ID if they make up a more complex object that should later be idenfied as a single object. 
+ *
+ * To identify an object based on the canvas position, use the function call `getObjectID(x,y)` which will return the ID number of the object closest to the camera at those (x,y) canvas coordinates or 0 if there is no object. The object ID at the mouse position can be simply determined with `objectAtMouse()`.
+ *
  * Functions:
  * The following methods are replaced with the corresponding functions listed below.
  * An asterisk indicates that the parameter list should be prepended with an integer
